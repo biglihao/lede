3f5e4753a4451b6cf32ccab53cda4b987640e1f7
biglihao <biglihao@gmail.com>
2023-03-25 10:23:38 +0800

    Update generic.mk

--- target/linux/ipq807x/image/generic.mk
+++ target/linux/ipq807x/image/generic.mk
@@ -91,6 +91,6 @@ define Device/redmi_ax6
         $(call Device/xiaomi_ax3600)
         DEVICE_VENDOR := Redmi
         DEVICE_MODEL := AX6
-        DEVICE_PACKAGES := ipq-wifi-redmi_ax6 uboot-envtools
+        DEVICE_PACKAGES := ipq-wifi-redmi_ax6
 endef
 TARGET_DEVICES += redmi_ax6
